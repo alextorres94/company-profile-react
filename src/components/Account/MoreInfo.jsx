b64ef9c3f7e58f74a7f2f909ca4747ef72741388
Alex Torres <atorres@finametrix.com>
2018-09-24 12:05:46 +0200

    Refactor some components

--- src/components/Account/MoreInfo.jsx
+++ src/components/Account/MoreInfo.jsx
@@ -12,53 +12,18 @@ const Title = styled.h4`
   color: #6ABB43;
 `;
 
-const StyledItems = styled(Col)`
-  display: flex;
-  flex-direction: row;
-    color: grey;
-
-`;
-
-const StyledValue = styled(Col)`
-  display: flex;
-  flex-direction: row;
-  color:red
-
-`;
-
 class MoreInfo extends Component {
 	render() {
-		const moreInfo = [{
-			uno: 'este es el uno',
-			dos: 'sssssl dosssss',
-			tres: 'el tresss',
-		}];
 		return (
 			<Container md={12}>
 				<Title>
 					{this.props.title}
 				</Title>
 				<Row>
-					{moreInfo.map((item) => {
-						<div>
-							<StyledItems md={6}>
-							{item}
-							</StyledItems>
-
-							<StyledValue md={6}>
-								{moreInfo[item]}
-							</StyledValue>
-						</div>
-					})}
-
+					Api info
 				</Row>
 			</Container>
 		);
-	}
-
-	renderLanguages() {
-		let languages =  this.props.languages;
-		return
 	}
 }
 
